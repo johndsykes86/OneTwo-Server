5eba6f9792f3e2399f016372c3be44b9c6f44871
Johnathon Sykes <hireme@mrjsykes.com>
2017-08-31 11:23:44 -0700

    added inital documentation to index path of auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,11 @@ const express = require('express'),
       authRouter = express.Router(),
       User = require('../models/User.js')
 
+var doc = "Welcome to the OneTwo API. If you have any questions or run into a technical issue, please email me at hireme@mrjsykes.com. "
+
 
 authRouter.get('/', (req,res)=>{
-  res.json({message: "this is the index route. If I were a better developer, I'd have a properly documented route"})
+  res.json({message: doc})
 })
 
 
