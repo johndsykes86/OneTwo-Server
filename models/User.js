862f977de89277ae32cf44c21e79ba1c89155f1b
Johnathon Sykes <hireme@mrjsykes.com>
2017-09-05 21:46:32 -0700

    added unique fields to email and userName

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose'),
       userSchema = new mongoose.Schema({
         firstName:{type: String, required: true},
         lastName:{type: String, required: true},
-        userName:{type: String, required: true},
-        email:{type: String, required: true},
+        userName:{type: String, required: true, unique: true},
+        email:{type: String, required: true, unique: true},
         password:{type: String, required: true,select:false},
         _checkIns:[
           {type: mongoose.Schema.Types.ObjectId, ref: 'CheckIn'}
